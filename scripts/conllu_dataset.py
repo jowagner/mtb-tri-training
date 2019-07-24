bc03642ead64cb335597386bbe6b5a5e95672dcb
Joachim Wagner <jowagner@users.noreply.github.com>
2019-07-24 16:17:52 +0100

    conllu reader and writer

--- scripts/conllu_dataset.py
+++ scripts/conllu_dataset.py
@@ -8,9 +8,15 @@
 
 # Author: Joachim Wagner
 
-from basic_dataset import Dataset
+import collections
 
-class Sentence(collections.Sequence):
+import basic_dataset
+
+id_column = 0
+token_column = 1
+pos_column = 3
+
+class ConlluSentence(basic_dataset.Sentence):
 
     def __init__(self):
         self.rows = []
@@ -26,25 +32,50 @@ class Sentence(collections.Sequence):
         fields = line.split('\t')
         if not fields:
             raise ValueError('cannot append empty line to conllu sentence')
+        # remove linebreak from last cell
         fields[-1] = fields[-1].rstrip()
+        #
         r_index = len(self.rows)
         self.rows.append(fields)
         # check whether this is a token
-        if fields[0].startswith('#'):
+        token_id = fields[id_column]
+        if token_id.startswith('#') \
+        or '-' in token_id \
+        or '.' in token_id:
             return
-        # TODO: more checks
-        raise NotImplementedError
+        #
+        self.token2row.append(r_index)
+
+    def clone(self):
+        copy = ConlluSentence()
+        for row in self.rows():
+            copy.append('\t'.join(row))
+        return copy
 
 
-class Conllu(Dataset):
+class Conllu(basic_dataset.Dataset):
 
     def __init__(self):
         Dataset.__init__(self)
-        self.column2vocab = {}
 
     def read_sentence(self, f_in):
-        raise NotImplementedError
+        sentence = None
+        while True:
+            line = f_in.readline()
+            if not line:
+                if sentence is not None:
+                    raise ValueError('unexpected end of file in conllu sentence')
+                break
+            elif sentence is None:
+                sentence = ConlluSentence()
+            if line.isspace():
+                break
+            sentence.append(line)
+        return sentence
 
     def write_sentence(self, f_out, sentence):
-        raise NotImplementedError
+        for row in sentence.rows():
+            # TODO: when to populate unlabelled target features with random labels?
+            f_out.write('\t'.join(row))
+            f_out.write('\n')
 
