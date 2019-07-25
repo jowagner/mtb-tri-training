4213484b814e91f69783502aaf97d2cd9da27cd5
Joachim Wagner <jowagner@users.noreply.github.com>
2019-07-25 16:34:56 +0100

    derive model seeds

--- scripts/tri-train.py
+++ scripts/tri-train.py
@@ -15,6 +15,8 @@ from __future__ import print_function
 
 import hashlib
 import os
+import random
+import string
 import sys
 
 def print_usage():
@@ -27,7 +29,7 @@ Options:
 
     --labelled  STRING
     --unlabelled  STRING    append data sets to the list of labelled or
-                            unlablled data sets; STRING must be a space- or
+                            unlabelled data sets; STRING must be a space- or
                             colon-separated list of data set IDs
 
     --seed  STRING          initialise random number generator with STRING;
@@ -43,51 +45,101 @@ Options:
                                         or digits)
                                 str24 = 2x str12
                                 compose = concatenation of our seed (--seed),
-                                    trainer rank (1-3) and training round,
+                                    learner rank (1-3) and training round,
                                     i.e. adding unique additional digits to the
                                     main seed
                             (default: do not provide the models with a seed,
                             usually meaning that they use a system seed)
 
-    --model-module  NAME    train models using the module NAME
-                            #(default: model_from_script)
-                            (default: model_from_script)
+    --dataset-module  NAME  handle data sets using the module NAME;
+                            combine with --help to see module-specific options
+                            (default: conllu_dataset)
 
-    --model-script  FILE    training script to use with model_from_script
-                            (default: train-
-
+    --model-module  NAME    train models using the module NAME;
+                            combine with --help to see module-specific options
+                            (default: uuparser_model)
 """)
 
-opt_help  = False
-test_type = 'dev'
-opt_debug = False
-opt_seed  = None
+def main():
+    opt_help  = False
+    test_type = 'dev'
+    opt_debug = False
+    opt_seed  = None
+    opt_labelled_ids = []
+    opt_unlabelled_ids = []
+    opt_dataset_module = 'conllu_dataset'
+    opt_model_module   = 'uuparser_model'
+    opt_model_seed     = None
 
-while len(sys.argv) >= 2 and sys.argv[1][:1] == '-':
-    option = sys.argv[1]
-    del sys.argv[1]
-    if option in ('--help', '-h'):
-        opt_help = True
-        break
-    elif option == '--test-type':
-        test_type = sys.argv[1]
+    while len(sys.argv) >= 2 and sys.argv[1][:1] == '-':
+        option = sys.argv[1]
         del sys.argv[1]
-    elif option == '--debug':
-        opt_debug = True
-    else:
-        print('Unsupported option %s' %option)
+        if option in ('--help', '-h'):
+            opt_help = True
+            break
+        elif option == '--test-type':
+            test_type = sys.argv[1]
+            del sys.argv[1]
+        elif option == '--model-seed':
+            opt_model_seed = sys.argv[1]
+            del sys.argv[1]
+        elif option in ('--labelled', '--unlabelled'):
+            for tbid in sys.argv[1].replace(':', ' ').split():
+                if option == '--labelled':
+                    opt_labelled_ids.append(tbid)
+                else:
+                    opt_unlabelled_ids.append(tbid)
+            del sys.argv[1]
+        elif option == '--debug':
+            opt_debug = True
+        else:
+            print('Unsupported option %s' %option)
+            opt_help = True
+            break
+
+    if len(sys.argv) != 1:
         opt_help = True
-        break
 
-if len(sys.argv) != 1:
-    opt_help = True
+    if opt_help:
+        print_usage()
+        sys.exit(0)
 
-if opt_help:
-    print_usage()
-    sys.exit(0)
+    if opt_seed:
+        random.seed(int(hashlib.sha512(opt_seed).hexdigest(), 16))
 
-if opt_seed:
-    random.seed(int(hashlib.sha512(opt_seed).hexdigest(), 16))
+    for training_round in range(5):
+        for learner_rank in (1,2,3):
+            print('%d\t%d\t%r' %(learner_rank, training_round,
+                get_model_seed(opt_model_seed, opt_seed, learner_rank, training_round)))
 
 
+def get_model_seed(mode, main_seed, learner_rank, training_round):
+    if mode in ('system', None):
+        return None
+    elif mode.startswith('int'):
+        bits = int(mode[3:])
+        if bits < 1:
+            raise ValueError('model seed must have at least 1 bit')
+        return '%d' %random.randrange(1, 2**bits)
+    elif mode.startswith('str'):
+        n = int(mode[3:])
+        if n < 1:
+            raise ValueError('model seed must have at least 1 character')
+        chars = []
+        chars.append(random.choice(string.ascii_letters))
+        n -= 1
+        while n > 0:
+            chars.append(random.choice(string.ascii_letters + string.digits))
+            n -= 1
+        return ''.join(chars)
+    elif mode == 'combine':
+        if main_seed is None:
+            raise ValueError('cannot combine model seed without main seed')
+        return '%s%d%d' %(main_seed, learner_rank, training_round)
+    else:
+        raise ValueError('unknown model seed type %r' %mode)
+
+
+if __name__ == "__main__":
+    main()
 
