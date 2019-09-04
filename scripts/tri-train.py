7c75e15332d6fe5d65fe67a3c518b581cbb7faab
Joachim Wagner <jowagner@users.noreply.github.com>
2019-09-04 18:29:15 +0100

    more options

--- scripts/tri-train.py
+++ scripts/tri-train.py
@@ -34,10 +34,11 @@ Options:
                             the module specified with --dataset-module
                             interprets these IDs to load the data
 
-    --seed  STRING          initialise random number generator with STRING;
+    --init-seed  STRING     initialise random number generator with STRING;
                             (default or empty string: use system seed)
 
-    --model-seed  TYPE      derive a seed for each model to be trained from
+    --model-init  TYPE      derive the initialisation seed for each model to
+                            be trained from
                             the output of this script's random number
                             generator; types:
                                 int15 = 15 bit non-zero number,
@@ -111,10 +112,22 @@ Options:
                             augment size.
                             (Default: 1 = pick subset at random)
 
-    --oversample            Oversample seed data to match size of 
+    --oversample            In each iteration and for each learner, oversample
+                            seed data to match size of unlabelled data.
+                            (Default: use seed data as is)
+
+    --iterations  NUMBER    Perform NUMBER iterations of tri-training.
+                            (Default: 5)
 
     --last-k  NUMBER        Only use the automatically labelled data of the
                             last k tri-training iterations
+                            (default: 0 = use all iterations)
+
+    --decay  NUMBER         Subsample (without replacement) fraction
+                            NUMBER^(j-i) of automatically labelled data of the
+                            i-th iteration in iteration j;
+                            can be combined with --last-k
+                            (default: 1.0 = use all data)
 
     --iteration-selection   Use development data to select strongest model for
                             each learner.
@@ -142,20 +155,30 @@ Options:
                             (default: abort if intermediate output files are
                             found)
 
-                            
+
 """)
 
 def main():
     opt_help  = False
     test_type = 'dev'
     opt_debug = False
-    opt_seed  = None
+    opt_init_seed  = None
     opt_labelled_ids = []
     opt_unlabelled_ids = []
     opt_dataset_module = 'conllu_dataset'
-    opt_model_module   = 'uuparser_model'
     opt_model_modules  = []
-    opt_model_seed     = None
+    opt_model_init_type = None
+    opt_seed_size = '100.0%'
+    opt_seed_attempts = 5
+    opt_subset_size = '20k'
+    opt_subset_attempts = 5
+    opt_augment_size = '4k'
+    opt_augment_attempts = 5
+    opt_diversify_attempts = 1
+    opt_oversample = False
+    opt_iterations = 5
+    opt_last_k = 0
+    opt_last_decay = 1.0
 
     while len(sys.argv) >= 2 and sys.argv[1][:1] == '-':
         option = sys.argv[1]
@@ -166,11 +189,11 @@ def main():
         elif option == '--test-type':
             test_type = sys.argv[1]
             del sys.argv[1]
-        elif option == '--seed':
-            opt_seed = sys.argv[1]
+        elif option == '--init-seed':
+            opt_init_seed = sys.argv[1]
             del sys.argv[1]
-        elif option == '--model-seed':
-            opt_model_seed = sys.argv[1]
+        elif option in ('--model-init', '--model-init-type'):
+            opt_model_init_type = sys.argv[1]
             del sys.argv[1]
         elif option in ('--labelled', '--unlabelled'):
             for tbid in sys.argv[1].replace(':', ' ').split():
@@ -178,6 +201,44 @@ def main():
                     opt_labelled_ids.append(tbid)
                 else:
                     opt_unlabelled_ids.append(tbid)
+            del sys.argv[1]
+        elif option == '--dataset-module':
+            opt_dataset_module = sys.argv[1]
+            del sys.argv[1]
+        elif option == '--model-module':
+            opt_model_modules.append(sys.argv[1])
+            del sys.argv[1]
+        elif option == '--seed-size':
+            opt_seed_size = sys.argv[1]
+            del sys.argv[1]
+        elif option == '--seed-attempts':
+            opt_seed_attempts = int(sys.argv[1])
+            del sys.argv[1]
+        elif option == '--subset-size':
+            opt_subset_size = sys.argv[1]
+            del sys.argv[1]
+        elif option == '--subset-attempts':
+            opt_subset_attempts = int(sys.argv[1])
+            del sys.argv[1]
+        elif option == '--augment-size':
+            opt_augment_size = sys.argv[1]
+            del sys.argv[1]
+        elif option == '--augment-attempts':
+            opt_augment_attempts = int(sys.argv[1])
+            del sys.argv[1]
+        elif option == '--diversify-attempts':
+            opt_diversify_attempts = int(sys.argv[1])
+            del sys.argv[1]
+        elif option == '--oversample':
+            opt_oversample = True
+        elif option == '--iterations':
+            opt_iterations = int(sys.argv[1])
+            del sys.argv[1]
+        elif option == '--last-k':
+            opt_last_k = int(sys.argv[1])
+            del sys.argv[1]
+        elif option in ('--last-decay', '--decay'):
+            opt_last_decay = float(sys.argv[1])
             del sys.argv[1]
         elif option == '--debug':
             opt_debug = True
@@ -193,14 +254,45 @@ def main():
         print_usage()
         sys.exit(0)
 
-    if opt_seed:
-        random.seed(int(hashlib.sha512(opt_seed).hexdigest(), 16))
+    if opt_last_k < 0:
+        raise ValueError('last_k must not be negative')
+    if opt_last_decay > 1.0:
+        raise ValueError('last_decay must not be greater than 1')
+    if opt_last_decay <= 0.0:
+        raise ValueError('last_decay must not be zero or negative')
 
-    for training_round in range(5):
+    if not opt_model_modules:
+        opt_model_modules.append('uuparser_model')
+
+    if opt_init_seed:
+        random.seed(int(hashlib.sha512(opt_init_seed).hexdigest(), 16))
+
+    training_data_size = training_data.get_number_of_items()
+    unlabelled_data_size = unlabelled_data.get_number_of_items()
+    opt_seed_size    = adjust_size(opt_seed_size,    training_data_size)
+    opt_subset_size  = adjust_size(opt_subset_size,  unlabelled_data_size)
+    opt_augment_size = adjust_size(opt_augment_size, unlabelled_data_size)
+
+    for training_round in range(opt_iterations):
         for learner_rank in (1,2,3):
             print('%d\t%d\t%r' %(learner_rank, training_round,
                 get_model_seed(opt_model_seed, opt_seed, learner_rank, training_round)))
 
+def adjust_size(size, data_size):
+    if size.endswith('%'):
+        fraction = float(size[:-1]) / 100.0
+        return int(0.5 + fraction * data_size)
+    elif opt_seed_size[-1:].lower() in 'kmgt':
+        fraction = float(size[:-1])
+        multiplier = {
+            'k': 1000.0,
+            'm': 10.0**6,
+            'g': 10.0**9,
+            't': 10.0**12,
+        }[size[-1:].lower()]
+        return int(0.5 + fraction * multiplier)
+    else:
+        return int(size)
 
 def get_model_seed(mode, main_seed, learner_rank, training_round):
     if mode in ('system', None):
