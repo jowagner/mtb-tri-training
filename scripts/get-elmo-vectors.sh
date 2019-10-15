6694dfc5464d1022fc2dd64c741d6ddb1ec98861
Joachim Wagner <jowagner@users.noreply.github.com>
2019-10-15 15:18:49 +0100

    correct tracking of age of elmo cache entries

--- scripts/get-elmo-vectors.sh
+++ scripts/get-elmo-vectors.sh
@@ -114,11 +114,11 @@ if [ -n "$EFML_CACHE_DIR" ]; then
         NUM_FILES=$(find ${EFML_CACHE_DIR}/ -name "*.size" | wc -l)
         if [ "$NUM_FILES" -gt "$EFML_MAX_CACHE_ENTRIES" ]; then
             PICK_FROM=$(expr ${EFML_MAX_CACHE_ENTRIES} / 2)
-            EXPIRED_ENTRY=$(ls -t ${EFML_CACHE_DIR}/ | fgrep .size | tail -n ${PICK_FROM} | shuf | head -n 1)
+            EXPIRED_ENTRY=$(ls -t ${EFML_CACHE_DIR}/ | fgrep .hdf5 | tail -n ${PICK_FROM} | shuf | head -n 1)
             rm ${EFML_CACHE_DIR}/${EXPIRED_ENTRY}
-            HDF5=$(basename ${EXPIRED_ENTRY} .size).hdf5
-            rm ${EFML_CACHE_DIR}/${HDF5}
-            cachelog "${EXPIRED_ENTRY} expired, cleaned up ${EXPIRED_ENTRY} and ${HDF5}"
+            SIZEFILE=$(basename ${EXPIRED_ENTRY} .hdf5).size
+            rm ${EFML_CACHE_DIR}/${SIZEFILE}
+            cachelog "${EXPIRED_ENTRY} expired, cleaned up"
         fi
     fi
 fi
