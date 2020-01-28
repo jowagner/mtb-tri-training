6a40576979e6cbf03e1845c3ee30ee750afcdfcb
Joachim Wagner <jowagner@users.noreply.github.com>
2020-01-28 09:13:43 +0000

    create inbox if needed; frequent updates in debug mode

--- scripts/common_udpipe_future.py
+++ scripts/common_udpipe_future.py
@@ -107,8 +107,9 @@ def run_command(command):
         num_task_buckets = int(os.environ['TT_TASK_BUCKETS'])
         task_fingerprint = hashlib.sha512(task_id).hexdigest()
         my_task_bucket = int(task_fingerprint, 16) % num_task_buckets
-        filename = '%s/udpf/inbox/%s-%d.task' %(
-            os.environ['TT_TASK_DIR'],
+        inbox_dir = os.environ['TT_TASK_DIR'] + '/udpf/inbox'
+        filename = '%s/%s-%d.task' %(
+            inbox_dir,
             task_id,
             my_task_bucket,
         )
@@ -117,6 +118,7 @@ def run_command(command):
         else:
             patience = 36000.0
         expires = now + patience
+        os.makedirs(inbox_dir, exist_ok=True)
         f = open(filename+'.prep', 'wb')
         f.write('expires %.1f\n' %expires)
         f.write('\n'.join(command))
@@ -130,6 +132,8 @@ def run_command(command):
         iteration = 0
         fp_length = len(task_fingerprint)
         verbosity_interval = 3600.0
+        if 'TT_DEBUG' in os.environ:
+            verbosity_interval /= 100.0
         next_verbose = submit_time + verbosity_interval
         start_time_interval = (submit_time, submit_time)
         while time.time() < expires and os.path.exists(filename):
@@ -162,6 +166,8 @@ def run_command(command):
             start_time = sum(start_time_interval) / 2.0
             # wait for task to finish (no timeout)
             verbosity_interval = 3600.0
+            if 'TT_DEBUG' in os.environ:
+                verbosity_interval /= 100.0
             next_verbose = min(next_verbose, start_time + verbosity_interval)
             while os.path.exists(filename):
                 duration = 30.0 + int(task_fingerprint[iteration % fp_length], 16)
