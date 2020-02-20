ef2bad8edcbca60e054eafdd27f98b80e68e8503
Joachim Wagner <jowagner@users.noreply.github.com>
2020-02-20 10:19:47 +0000

    need udpf environment on cpu node for elmo cache

--- config/locations.sh
+++ config/locations.sh
@@ -105,6 +105,8 @@ case "${SETTING}" in
     export PRJ_DIR=${HOME}/tri-training/mtb-tri-training
     export UD_TREEBANK_DIR=${HOME}/data/ud-treebanks-v2.3
     export CONLL2017_DIR=${HOME}/data/conll2017/clean
+    export UDPIPE_FUTURE_DIR=${HOME}/tri-training/UDPipe-Future
+    export UDPIPE_FUTURE_ENV=${UDPIPE_FUTURE_DIR}/venv-udpf
     export CONLLU_COMBINER_DIR=${HOME}/tri-training/ADAPT-DCU/combination
     export EFML_NPZ_CACHE_DIR=${HOME}/data/elmo-cache
     export EFML_NPZ_CACHE_SIZE=120GiB
