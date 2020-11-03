43942c80514dd5e2e83aaffd8d29575544d8c435
Joachim Wagner <jowagner@users.noreply.github.com>
2020-11-03 10:35:24 +0000

    create tables comparing effect of seed sampling

---
+++ scripts/distribution.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# (C) 2019, 2020 Dublin City University
+# All rights reserved. This material may not be
+# reproduced, displayed, modified or distributed without the express prior
+# written permission of the copyright holder.
+
+# Author: Joachim Wagner
+
+import os
+import sys
+
+class Distribution:
+
+    def __init__(self, language, parser, sample, smooth = True):
+        self.smooth = smooth
+        filename = None
+        if language is None:
+            self.scores = True
+            self.min_score =   0.0
+            self.score025  =   2.5
+            self.score250  =  25.0
+            self.median    =  50.0
+            self.score750  =  75.0
+            self.score975  =  97.5
+            self.max_score = 100.0
+            return
+        if sample == '-':
+            sample = 's'
+        if language == 't':
+            parser = 't'
+        code = language + parser + sample
+        for entry in os.listdir(os.environ['TT_DISTRIBUTIONS_DIR']):
+            if not entry.startswith('distribution-') \
+            or not entry.endswith('-dev.txt'):
+                continue
+            fields = entry.split('-')
+            if len(fields) != 5 \
+            or fields[1] != code \
+            or fields[2] != '3':
+                continue
+            filename = entry
+            break
+        self.scores = []
+        scores = self.scores
+        if not filename:
+            sys.stderr.write('Warning: no baseline distribution for %s.\n' %code)
+        else:
+            f = open('%s/%s' %(os.environ['TT_DISTRIBUTIONS_DIR'], filename), 'rb')
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                scores.append(float(line.split()[0]))
+            f.close()
+        if self.scores:
+            self.average = sum(scores) / float(len(scores))
+            scores.sort()
+            self.min_score = scores[0]
+            self.max_score = scores[-1]
+            num_scores = len(scores)
+            if (num_scores % 2):
+                # odd number of scores
+                self.median = scores[(num_scores-1)/2]
+            else:
+                # even number of scores
+                self.median = (scores[num_scores/2-1] + scores[num_scores/2])/2.0
+            backup = scores
+            prune = int(0.025*len(scores))
+            if prune:
+                scores = self.scores[prune:-prune]
+            self.score025 = scores[0]
+            self.score975 = scores[-1]
+            scores = backup
+            prune = int(0.25*len(scores))
+            if prune:
+                scores = self.scores[prune:-prune]
+            self.score250 = scores[0]
+            self.score750 = scores[-1]
+
+    def colour(self, score):
+        if not self.scores:
+            return 'ffffff'
+        if self.smooth:
+            indices = range(101)
+        else:
+            indices = [50,]
+        colours = []
+        for i in indices:
+            for f in (0.000023, 0.000011):
+                offset = f * (i - 50)
+                colours.append(self.p_colour(score+offset))
+        components = []
+        for c_index in (0,1,2):
+            value = 0.0
+            for colour in colours:
+                value += colour[c_index]
+            components.append('%02x' %int(255.999 * value / float(len(colours))))
+        return ''.join(components)
+
+    def p_colour(self, score):
+        if score < self.min_score:
+            return (0.00, 0.00, 0.00) # below 0.0: black
+        if score < self.score025:
+            return (0.60, 0.30, 0.15) #  0.0 -  2.5: brown
+        if score < self.score250:
+            return (0.67, 0.52, 0.45) #  2.5 - 25.0: brown-grey
+        if score < self.median:
+            return (0.75, 0.75, 0.75) # 25.0 - 50.0: grey
+        if score < self.score750:
+            return (1.00, 1.00, 1.00) # 50.0 - 75.0: white
+        if score < self.score975:
+            return (1.00, 1.00, 1.00) # 75.0 - 97.5: white
+        if score < self.max_score:
+            #return (0.80, 0.60, 1.00) # 97.5 - 100: light violet-blue
+            return (0.65, 0.65, 1.00) # 97.5 - 100: light blue
+        #if score < self.max_score+0.4:
+            #return (0.65, 0.65, 1.00) # light blue
+        if score < self.max_score+0.6:
+            return (0.6, 1.0, 1.0)  # light cyan-blue
+        if score < self.max_score+1.2:
+            return (0.80, 1.0, 0.50)  # light yellow-green
+        if score < self.max_score+1.8:
+            return (1.0, 1.0, 0.0)  # strong yellow
+        if score < self.max_score+2.4:
+            return (1.0, 0.8, 0.5)  # orange-pink
+        else:
+            return (1.0, 0.5, 0.5)  # light red
+
+def main():
+    language = sys.argv[1]
+    parser   = sys.argv[2]
+    x = 1.0
+    for sample in '-wxt':
+        distr = Distribution(language, parser, sample)
+        if not distr.scores:
+            continue
+        sys.stdout.write('# sampling: %s\n' %sample)
+        sys.stdout.write('%.1f\t%.9f\t# min\n'     %(x, distr.min_score))
+        sys.stdout.write('%.1f\t%.9f\t# 2.5%%\n'   %(x+0.5, distr.score025))
+        sys.stdout.write('%.1f\t%.9f\t# 25%%\n'    %(x+1.0, distr.score250))
+        sys.stdout.write('%.1f\t%.9f\t# median\n'  %(x+2.0, distr.median))
+        sys.stdout.write('%.1f\t%.9f\t# 75%%\n'    %(x+3.0, distr.score750))
+        sys.stdout.write('%.1f\t%.9f\t# 97.5%%\n'  %(x+3.5, distr.score975))
+        sys.stdout.write('%.1f\t%.9f\t# max\n'     %(x+4.0, distr.max_score))
+        sys.stdout.write('%.1f\t%.9f\t# average\n' %(x+4.5, distr.average))
+        x += 10.0
+
+if __name__ == "__main__":
+    main()
+
---
+++ scripts/scores-to-seed-distr.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# (C) 2020 Dublin City University
+# All rights reserved. This material may not be
+# reproduced, displayed, modified or distributed without the express prior
+# written permission of the copyright holder.
+
+# Author: Joachim Wagner
+
+import os
+import sys
+
+from distribution import Distribution
+
+target_parsers = 'fh'
+target_samples = 'wx'
+target_min_rounds = 4   # do not include a run if it has fewer rounds
+
+if len(target_parsers) < 2:
+    target_parsers = target_parsers + target_parsers[0]
+
+assert len(target_samples) == 2
+
+header = sys.stdin.readline().split()
+
+baseline_column = header.index('0')
+language_column = header.index('Language')
+parser_column   = header.index('Parser')
+try:
+    run_column  = header.index('Run')
+except:
+    run_column  = None
+aug_column      = header.index('AugmentSizeIndex')
+method_column   = header.index('Method')
+learner_column  = header.index('Learner')
+testset_column  = header.index('TestSetIndex')
+rounds_column   = header.index('Rounds')
+
+example_header = """Language        Parser  Run     AugmentSizeIndex        Method  NumberOfLearners        Learner TestSetIndex    Rounds  UnlabelledTokensInLastRound    0       1
+"""
+
+def unpack_score(score):
+    if ':' in score:
+        score, date, tokens, sentences = score.split(':')[:4]
+    else:
+        date, tokens, sentences = '????-??-??', '-1', '-1'
+    return float(score), date, int(tokens), int(sentences)
+
+graphs = {}
+languages = set()
+parsers = set()
+sample_types = set()
+
+while True:
+    line = sys.stdin.readline()
+    if not line:
+        break
+    row = line.split()
+    if row[learner_column] != 'Ensemble':
+        continue
+    if row[testset_column] != '0':
+        continue
+    # find best and worst baseline scores
+    language = row[language_column]
+    parser   = row[parser_column]
+    if parser == 't':
+        parser = 'f'
+    if parser not in target_parsers:
+        continue
+    method   = row[method_column]
+    oversampling, sample, agreement, decay = method
+    try:
+        augsize = row[aug_column]
+    except:
+        raise ValueError('No index %d in row %r' %(aug_column, row))
+    if run_column is None:
+        run = '1'
+    else:
+        run = row[run_column]
+    n_rounds = int(row[rounds_column])
+    if n_rounds < target_min_rounds:
+        continue
+    languages.add(language)
+    parsers.add(parser)
+    sample_types.add(sample)
+    graph_key = (language, parser, sample)
+    setting_key = (oversampling, agreement, decay, augsize)
+    if not graph_key in graphs:
+        graphs[graph_key] = {}
+    graph = graphs[graph_key]
+    if not setting_key in graph:
+        graph[setting_key] = []
+    setting = graph[setting_key]
+    experiment_code = '%s%s%s%s%s%s%d%s' %(
+        language, parser, oversampling, sample,
+        agreement, decay,
+        2 + int(run),
+        augsize
+    )
+    setting.append((-n_rounds, run, experiment_code, row))
+
+summary = []
+for lang_index, language in enumerate(sorted(list(languages))):
+    sys.stderr.write('\n\n== Language %s ==\n\n' %language)
+    eligible_setting_keys = None
+    for parser in target_parsers:
+        for sample in target_samples:
+            graph_key = (language, parser, sample)
+            if not graph_key in graphs:
+                setting_keys_for_graph_key = set()
+            else:
+                setting_keys_for_graph_key = set(graphs[graph_key].keys())
+            if eligible_setting_keys is None:
+                eligible_setting_keys = setting_keys_for_graph_key
+            else:
+                # intersection
+                eligible_setting_keys = eligible_setting_keys.intersection(setting_keys_for_graph_key)
+    if not eligible_setting_keys:
+        continue
+    sys.stderr.write('Usable settings:\n')
+    sk2min_rounds = {}
+    for setting_key in sorted(list(eligible_setting_keys)):
+        min_rounds = []
+        for run_index in (0,1):
+            rounds = []
+            for parser in target_parsers:
+                for sample in target_samples:
+                    graph_key = (language, parser, sample)
+                    setting = graphs[graph_key][setting_key]
+                    if not run_index:
+                        setting.sort()
+                    if len(setting) > run_index:
+                        neg_n_rounds, run, exp_code, row = setting[run_index]
+                        rounds.append(-neg_n_rounds)
+            if len(rounds) == 4:
+                assert len(min_rounds) == run_index 
+                min_rounds.append(min(rounds))
+        #setting = ( # (-int(n_rounds), run, experiment_code, row)
+        sys.stderr.write('\t%s?%s%s%s\t%r\n' %(tuple(setting_key) + (min_rounds,)))
+        sk2min_rounds[setting_key] = min_rounds
+    for parser in target_parsers:
+        # get list of columns for header
+        exp_codes = []
+        for sample in '-wxt':
+            graph_key = (language, parser, sample)
+            if not graph_key in graphs:
+                assert sample not in target_samples
+                continue
+            graph = graphs[graph_key]
+            for setting_key in sorted(list(eligible_setting_keys)):
+                if not setting_key in graph:
+                    assert sample not in target_samples
+                    continue
+                setting = graph[setting_key]
+                min_rounds = sk2min_rounds[setting_key]
+                for run_index, rounds in enumerate(min_rounds):
+                    if sample not in target_samples:
+                        setting.sort()
+                    if len(setting) > run_index:
+                        neg_n_rounds, run, exp_code, row = setting[run_index]
+                        exp_codes.append(exp_code)
+        out = open('out_%s_%s.tsv' %(language, parser), 'wb')
+        # write header
+        header = []
+        header.append('X')
+        for column in 'Average Min P025 P250 Median P750 P975 Max'.split():
+            header.append(column)
+        for exp_code in exp_codes:
+            header.append(exp_code)
+        out.write('%s\n' %('\t'.join(header)))
+        # prepare table contents
+        sys.stderr.write('\nParser %s:\n' %parser)
+        x_base = 1.0
+        characteristic = []
+        for sample in '-wxt':
+            distr = Distribution(language, parser, sample)
+            if distr.scores:
+                for (x, value, column) in [
+                    (-0.7, distr.average,  0),
+                    (-0.3, distr.min_score, 1),
+                    (-0.3, distr.score025,  2),
+                    (-0.3, distr.score250,  3),
+                    (-0.3, distr.median,    4),
+                    (-0.3, distr.score750,  5),
+                    (-0.3, distr.score975,  6),
+                    (-0.3, distr.max_score, 7),
+                ]:
+                    output_row = []
+                    output_row.append('%.1f' %(x_base + x))
+                    for _ in range(column):
+                        output_row.append('')
+                    output_row.append('%.9f' %value)
+                    out.write('%s\n' %('\t'.join(output_row)))
+            graph_key = (language, parser, sample)
+            if not graph_key in graphs:
+                assert sample not in target_samples
+                x_base = x_base + 2.0
+                continue
+            graph = graphs[graph_key]
+            max_rounds = 0
+            best_scores = []
+            for setting_key in sorted(list(eligible_setting_keys)):
+                if not setting_key in graph:
+                    assert sample not in target_samples
+                    continue
+                setting = graph[setting_key]
+                min_rounds = sk2min_rounds[setting_key]
+                for run_index, rounds in enumerate(min_rounds):
+                    if rounds > max_rounds:
+                        max_rounds = rounds
+                    if len(setting) > run_index:
+                        neg_n_rounds, run, exp_code, row = setting[run_index]
+                        column = exp_codes.index(exp_code)
+                        best_score = 0.0
+                        scores_tested = 0
+                        for t, info in enumerate(row[baseline_column:]):
+                            if t > rounds:
+                                continue
+                            score, _, _, _ = unpack_score(info)
+                            x = x_base + t
+                            output_row = []
+                            output_row.append('%.2f' %x)
+                            for _ in range(column+8):
+                                output_row.append('')
+                            output_row.append('%.9f' %score)
+                            out.write('%s\n' %('\t'.join(output_row)))
+                            if score > best_score:
+                                best_score = score
+                            scores_tested = scores_tested + 1
+                        best_scores.append(best_score)
+                        sys.stderr.write('\t%s?%s%s%s\t%s\t%d\t%.9f\t(%d scores)\n' %(tuple(setting_key) + (sample, run_index, best_score, scores_tested)))
+            if best_scores:
+                average_score = sum(best_scores) / float(len(best_scores))
+                sys.stderr.write('\taverage best score for %s: %.9f (%d scores)\n' %(sample, average_score, len(best_scores)))
+            if sample in target_samples:
+                assert len(best_scores) > 0
+                average_score = sum(best_scores) / float(len(best_scores))
+                characteristic.append(average_score)
+            x_base = x_base + max_rounds + 2
+        out.close()
+        assert len(characteristic) == 2
+        summary_row = []
+        summary_row.append(language)
+        summary_row.append(parser)
+        summary_row.append('%.2f' %(characteristic[0]))
+        summary_row.append('%.2f' %(characteristic[1]))
+        summary_row.append('%.2f' %(characteristic[1]-characteristic[0]))
+        summary.append('\t'.join(summary_row))
+
+header = []
+header.append('Language')
+header.append('Parser')
+for sample in target_samples:
+    header.append('LAS-with-%s-sampling' %sample)
+header.append('Improvement')
+out = open('summary.tsv', 'wb')
+out.write('\t'.join(header))
+out.write('\n')
+for row in summary:
+    out.write(row+'\n')
+out.close()
