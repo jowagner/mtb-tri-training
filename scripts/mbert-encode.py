b05f13757d2f58421945021852061107176bea72
Joachim Wagner <jowagner@users.noreply.github.com>
2021-04-06 12:59:46 +0100

    debugged batch encoding

--- scripts/mbert-encode.py
+++ scripts/mbert-encode.py
@@ -11,8 +11,10 @@
 # adapted from https://huggingface.co/transformers/quickstart.html
 
 import logging
+import random
+import sys
 import torch
-from   transformers import BertTokenizer, BertModel
+from   transformers import BertTokenizerFast, BertModel
 
 import conllu_dataset
 
@@ -20,14 +22,43 @@ opt_verbose = True
 opt_debug   = True
 opt_use_gpu = False
 opt_max_sequence_length = 512
+opt_input_format = 'auto-detect'
+opt_output_layer = -1
+opt_batch_size = 64
+opt_pooling = 'first'   # one of first, last, max and average
 
 if opt_debug:
     opt_max_sequence_length = 10
+    opt_input_format = 'line'
+    opt_batch_size = 4
+
+specials_and_replacements = [
+    # in the unlikely event that one of the special tokens appears in input,
+    # we replace it with a similar token
+    ('[CLS]',  '[CLS2]'),
+    ('[SEP]',  '[SEP2]'),
+    ('[UNK]',  '[UNK2]'),
+    ('[MASK]', '[MASK2]'),
+]
+
+filename = sys.argv[1]
+if filename == '-':
+    infile = sys.stdin
+    opt_input_format = 'line'
+else:
+    infile = open(filename, 'rt')
+    if opt_input_format == 'auto-detect':
+        if filename.endswith('.conllu'):
+            opt_input_format = 'conll'
+        elif filename.endswith('.txt'):
+            opt_input_format = 'line'
+        else:
+            raise ValueError('Cannot autodetect file format')
 
 if opt_verbose:
     logging.basicConfig(level=logging.INFO)
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizerFast.from_pretrained('bert-base-multilingual-cased')
 model     = BertModel.from_pretrained('bert-base-multilingual-cased')
 model.eval()  # evaluation mode without dropout
 
@@ -37,40 +68,64 @@ if opt_use_gpu:
 def encode_batch(batch):
     if opt_debug: print('creating batch for %d sequences' %len(batch))
     s_idxs, p_idxs, is_last, sentences = zip(*batch)
+    print('sequences to be encoded:', sentences)
     encoded_batch = tokenizer(
-        sentences, # pre-tokenised input
+        list(sentences), # pre-tokenised input
         is_split_into_words = True,
+        return_tensors      = 'pt',
+        #add_special_tokens  = True,  # TODO: no effect on output and not clear from documentation what it does
+        padding             = True,
     )
-    encoded_batch['sentence_indices'] = s_idxs
-    encoded_batch['part_indices']     = p_idxs
-    encoded_batch['is_last']          = is_last
-    if opt_debug: print_encoded_batch(sentences, encoded_batch)
+    if opt_debug:
+        print_encoded_batch(sentences, encoded_batch, s_idxs, p_idxs, is_last)
     if opt_use_gpu:
         encoded_batch.to('cuda')
-    return encoded_batch
+    return (s_idxs, p_idxs, is_last, encoded_batch)
 
-def print_encoded_batch(sentences, encoded_batch):
+def print_encoded_batch(sentences, encoded_batch, s_idxs, p_idxs, is_last):
     global tokenizer
     print('batch:')
-    for b_index, sentence in sentences:
+    for b_index, sentence in enumerate(sentences):
         print('[%d]' %b_index)
-        print('\ts_idx:', encoded_batch['sentence_indices'][b_index])
-        print('\tp_idx:', encoded_batch['part_indices'][b_index])
-        print('\tis_last:', encoded_batch['is_last'][b_index])
+        print('\ts_idx:', s_idxs[b_index])
+        print('\tp_idx:', p_idxs[b_index])
+        print('\tis_last:', is_last[b_index])
         print('\tsentence:', sentences[b_index])
-        token_ids = encoded_bactch['input_ids'][b_index]
+        token_ids = encoded_batch['input_ids'][b_index]
         print('\tsubword units:', tokenizer.convert_ids_to_tokens(token_ids))
-        print('\t.word_ids():', encoded_bactch.word_ids(batch_index = i))
+        print('\t.word_ids():', encoded_batch.word_ids(batch_index = b_index))
 
+def protect_special(sentence):
+    global specials_and_replacements
+    global opt_debug
+    text = ' '.join(sentence)
+    replacement_required = False
+    for special, _ in specials_and_replacements:
+        if special in text:
+            replacement_required = True
+            break
+    if not replacement_required:
+        return sentence
+    if opt_debug: print('protecting special token(s)')
+    new_sentence = []
+    for token in sentence:
+        for special, replacement in specials_and_replacements:
+            token = token.replace(special, replacement)
+        new_sentence.append(token)
+    return new_sentence
 
-def get_batches(conllu_file, batch_size = 64, buffer_batches = 2, shuffle_buffer = False):
+def get_batches(conllu_file, batch_size = None, buffer_batches = 2, shuffle_buffer = False):
     global tokenizer
     global opt_debug
+    global opt_batch_size
+    if not batch_size:
+        batch_size = opt_batch_size
     s_buffer = []
     buffer_size = batch_size * buffer_batches
     s_index = 0
     for sentence in get_sentences(conllu_file):
         if opt_debug: print('\nsentence %d: %s' %(s_index+1, sentence))
+        sentence = protect_special(sentence)
         # first try to fit the full input into BERT, i.e. set the
         # split point to after the input
         split_point = len(sentence)
@@ -123,6 +178,14 @@ def get_batches(conllu_file, batch_size = 64, buffer_batches = 2, shuffle_buffer
         s_buffer = s_buffer[batch_size:]
 
 def get_sentences(conllu_file):
+    global opt_input_format
+    if opt_input_format == 'line':
+        for line in conllu_file.readlines():
+            tokens = line.split()
+            if tokens:
+                yield tokens
+        return
+    assert opt_input_format == 'conll'
     while True:
         conllu = conllu_dataset.ConlluDataset()
         sentence = conllu.read_sentence(conllu_file)
@@ -137,21 +200,25 @@ data = []
 s2n_parts = {}
 sp2vectors = {}
 with torch.no_grad():
-    for encoded_batch in get_batches(conllu_file):
-        outputs = model(encoded_batch)
-        last_layer = outputs[0]
+    for s_idxs, p_idxs, is_lasts, encoded_batch in get_batches(infile):
+        outputs = model(**encoded_batch)
+        batch_of_last_layers = outputs[0]
         # this should have shape (batch size, sequence length, model hidden dimension)
-        for index, vectors in last_layer:
-            s_index = encoded_batch['sentence_indices'][index]
-            p_index = encoded_batch['part_indices'][index]
-            is_last = encoded_batch['is_last'][index]
+        for index, vectors in enumerate(batch_of_last_layers):
+            s_index = s_idxs[index]
+            p_index = p_idxs[index]
+            is_last = is_lasts[index]
             if is_last:
                 s2n_parts[s_index] = p_index + 1  # record number of parts
             key = (s_index, p_index)
             sp2vectors[key] = vectors
+            if opt_debug:
+                print('s2n_parts:', s2n_parts)
             # do we have all parts for this sentence?
             if s_index not in s2n_parts:
                 # no as we haven't seen the last part yet
+                if opt_debug:
+                    print('still missing last part of sentence', s_index+1)
                 continue
             expected_n_parts = s2n_parts[s_index]
             assert expected_n_parts > 0
@@ -163,13 +230,16 @@ with torch.no_grad():
                     break
             if not found_all:
                 # some other part is missing
+                if opt_debug:
+                    print('still missing some part(s) of sentence', s_index+1)
                 continue
             # all parts ready
             if opt_debug: print('all parts ready for sentence', s_index+1)
             # TODO: concatenate vectors, reduce to tokens (pick first or pool)
             #       and add to hdf5
-            raise NotImplementedError
-            for token in example:
+            assert opt_output_layer == -1
+            assert opt_pooling == 'first'   # one of first, last, max and average
+            for token in []: # example:
                 # https://stackoverflow.com/questions/62317723/tokens-to-words-mapping-in-the-tokenizer-decode-step-huggingface
                 print('%18s --> %r' %(
                     token,
@@ -183,7 +253,11 @@ with torch.no_grad():
                 key = (s_index, p_index)
                 del sp2vectors[key]
             del s2n_parts[s_index]
+        if opt_debug:
+           print('finished batch')
+           print()
 
 # TODO: write efml-like hdf5 file
-raise NotImplementedError
 
+if filename != '-':
+    infile.close()
