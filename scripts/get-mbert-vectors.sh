a5b039de5190e08e535e61d6d10de254a3027cad
Joachim Wagner <jowagner@users.noreply.github.com>
2021-04-15 16:20:05 +0100

    support changing mbert parameters in experiments

--- scripts/get-mbert-vectors.sh
+++ scripts/get-mbert-vectors.sh
@@ -9,22 +9,29 @@ test -z $1 && echo "Missing training conllu file"
 test -z $1 && exit 1
 TRAIN_CONLLU=$1
 
-test -z $2 && echo "Missing output folder"
+test -z $2 && echo "Missing layer specification"
 test -z $2 && exit 1
-OUTPUTDIR=$2
+LAYER=$2
 
-test -z $3 && echo "Missing output filename"
+test -z $3 && echo "Missing expand_to specification"
 test -z $3 && exit 1
-HDF5_NAME=$3
+EXPAND_TO=$3
+
+test -z $4 && echo "Missing pooling specification"
+test -z $4 && exit 1
+POOLING=$4
+
+test -z $5 && echo "Missing output folder"
+test -z $5 && exit 1
+OUTPUTDIR=$5
+
+test -z $6 && echo "Missing output filename"
+test -z $6 && exit 1
+HDF5_NAME=$6
 
 test -z ${PRJ_DIR} && PRJ_DIR=${HOME}/mtb-tri-training
 
 source ${PRJ_DIR}/config/locations.sh
-
-cachelog()
-{
-    echo `date +%Y-%m-%dT%H:%M:%S` "["$(hostname):$$"]" $* >> ${MBERT_HDF5_CACHE_DIR}/log
-}
 
 if [ -n "$MBERT_LIB_PATH" ]; then
     export LD_LIBRARY_PATH=${MBERT_LIB_PATH}:${LD_LIBRARY_PATH}
@@ -46,8 +53,6 @@ fi
 
 hostname > ${OUTPUTDIR}/mbert.start
 
-LAYER=-1
-
 INFILE=$(realpath ${TRAIN_CONLLU})
 
 TMP_OUTFILE=${OUTPUTDIR}/${HDF5_NAME}_part
@@ -68,6 +73,8 @@ if ! python scripts/mbert-encode.py \
     ${MBERT_OPTIONS}                \
     --input-format conll            \
     --output-layer ${LAYER}         \
+    --expand-to ${EXPAND_TO}        \
+    --pooling ${POOLING}            \
     ${INFILE}                       \
     ${TMP_OUTFILE}
 then
