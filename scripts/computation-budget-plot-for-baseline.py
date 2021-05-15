d027d606d70e6edd8f4d4e359bcd4d5c8a0d3ad7
Joachim Wagner <jowagner@users.noreply.github.com>
2021-05-15 17:14:54 +0100

    create model samples and get best LAS for each budget

--- scripts/computation-budget-plot-for-baseline.py
+++ scripts/computation-budget-plot-for-baseline.py
@@ -13,14 +13,19 @@
 
 from __future__ import print_function
 
-from collections import defaultdict
+import hashlib
 import random
 import sys
+import time
 
 from distribution import Distribution
 
+start_t = time.time()
+
 language = sys.argv[1]
 number_of_bins = int(sys.argv[2])
+number_of_samples = int(sys.argv[3])
+random.seed(int(hashlib.sha256(sys.argv[4]).hexdigest(), 16))
 
 l2text = {
     'e': 'English',
@@ -56,6 +61,8 @@ d2text = {
     'u': 'vanilla, using all labelled data',
     'r': 'decaying with factor 0.50, using all labelled data',
 }
+
+print('Language:', l2text[language])
 
 parsers = list(p2text.keys())
 
@@ -101,6 +108,8 @@ def get_budget_and_best_score(selection):
     total = 0.0
     for duration in budget:
         total += duration
+    # convert seconds to days
+    total /= (24*3600)
     return (total, best_score)
 
 total_budget, highest_score = get_budget_and_best_score(available_ensembles)
@@ -121,7 +130,9 @@ print('score range:', lowest_score, 'to', highest_score, '(ratio %.2f)' %score_r
 
 # prepare the budget points for which we output score distributions
 
-bin_budget_ratio = total_budget/(smallest_budget+12.3)
+print('Number of bins:', number_of_bins)
+
+bin_budget_ratio = budget_ratio/1.001   # set lowest ceiling slightly above lowest budget
 bin_ceiling_multiplier = bin_budget_ratio**(1.0/(number_of_bins-1))
 bin_ceilings = []
 next_bin_ceiling = total_budget
@@ -131,8 +142,106 @@ for _ in range(number_of_bins):
 
 bin_ceilings.sort()
 print('Bins:')
+header = 'index floor ceiling'.split()
+print('\t'.join(header))
 last_ceiling = 0.0
 for index in range(number_of_bins):
     ceiling = bin_ceilings[index]
-    print('[%d] %.3f to %.3f' %(index, last_ceiling, ceiling))
+    print('%d\t%15.5f\t%15.5f' %(index, last_ceiling, ceiling))
     last_ceiling = ceiling
+
+# collect samples
+
+print('Number of samples:', number_of_samples)
+
+# initialise stats
+ceiling2scores = {}
+for ceiling in bin_ceilings:
+    ceiling2scores[ceiling] = []
+
+last_run_with_progress = 0
+current_run = 0
+last_state = None
+last_verbose = 0.0
+def satisfied_with_samples(ceiling2scores, target_n):
+    global last_run_with_progress
+    global current_run
+    global last_state
+    global bin_ceilings
+    global last_verbose
+    current_run += 1
+    current_state = []
+    highest_non_ready = -1
+    for ceiling in bin_ceilings:
+        ready = len(ceiling2scores[ceiling])
+        current_state.append(ready)
+        if ready < target_n and ready > highest_non_ready:
+            highest_non_ready = ready
+    current_state = tuple(current_state)
+    now = time.time()
+    if now > last_verbose + 1.0:
+        print('Run %d: %d of %d bins ready, highest non-ready bin has %.1f%% of samples' %(
+            current_run, current_state.count(target_n), len(current_state),
+            100.0 * highest_non_ready / float(target_n))
+        )
+        sys.stdout.flush()
+        last_verbose = now
+    if current_state != last_state:
+        last_run_with_progress = current_run
+        last_state = current_state
+    if min(current_state) == target_n:
+        return True
+    if current_run - last_run_with_progress > 500:
+        # not making progress, give up
+        return True
+    return False
+
+# main loop
+while not satisfied_with_samples(ceiling2scores, number_of_samples):
+    random.shuffle(available_ensembles)
+    selection = available_ensembles[:]  # clone
+    for ceiling in reversed(bin_ceilings):
+        if len(ceiling2scores[ceiling]) >= number_of_samples:
+            # collected enough samples for this ceiling
+            continue
+        # check that ceiling is feasible with current shuffle
+        min_budget, _ = get_budget_and_best_score([selection[0]])
+        if min_budget > ceiling:
+            break
+        n_selected = len(selection)
+        if get_budget_and_best_score(selection)[0] > ceiling:
+            # budget of current selection is too high
+            # --> perform a binary search for the highest value of
+            #     n_selected such that budget fits into ceiling
+            lower = 1
+            while n_selected - lower > 1:
+                m_selected = (lower+n_selected)//2
+                candidate = selection[:m_selected]
+                budget, score = get_budget_and_best_score(candidate)
+                if budget <= ceiling:
+                    lower = m_selected
+                else:
+                    n_selected = m_selected
+            n_selected = lower
+            selection = selection[:n_selected]
+        assert n_selected > 0
+        budget, score = get_budget_and_best_score(selection)
+        assert budget <= ceiling
+        ceiling2scores[ceiling].append(score)
+
+print('Scores in each bin:')
+header = 'index ceiling n min avg max'.split()
+print('\t'.join(header))
+for index in range(number_of_bins):
+    ceiling = bin_ceilings[index]
+    scores = ceiling2scores[ceiling]
+    if scores:
+        n = len(scores)
+        min_score = min(scores)
+        avg_score = sum(scores)/float(n)
+        max_score = max(scores)
+        print('%d\t%15.5f\t%d\t%7.2f\t%7.2f\t%7.2f' %(index, ceiling, n, min_score, avg_score, max_score))
+    else:
+        print('%d\t%15.5f\t0' %(index, ceiling))
+
+print('Duration:', time.time()-start_t)
